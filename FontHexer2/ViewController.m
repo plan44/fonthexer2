958a672f05fb0e5aebaf8055645c827a9bc81f7c
Lukas Zeller <luz@plan44.ch>
2023-12-20 17:55:54 +0100

    Sampling works (manually fixed)

--- FontHexer2/ViewController.m
+++ FontHexer2/ViewController.m
@@ -92,130 +92,82 @@
 
 - (IBAction)samplePixels:(id)sender
 {
-  [self sampleColorsInGridCells];
+  NSArray* glyph = [self sampleColorsInGridCells];
+  NSLog(@"Glyph with %ld cols", glyph.count);
 }
 
 
 // MARK: sample
 
-#if GUGUS
-- (void)sampleColorsInGridCells {
-    // Get the bounds of the view
-    NSRect bounds = [self bounds];
-
-    // Get the ViewController
-    ViewController *viewController = (ViewController *)self.window.contentViewController;
-
-    // Create the bitmap representation
-    NSImage *image = [[NSImage alloc] initWithSize:self.label.bounds.size];
-
-    // Create a graphics context with a scale of 1.0
-    NSGraphicsContext *context = [NSGraphicsContext graphicsContextWithBitmapImageRep:[[NSBitmapImageRep alloc] initWithData:[image TIFFRepresentation]]];
-    [NSGraphicsContext saveGraphicsState];
-    [NSGraphicsContext setCurrentContext:context];
-    CGContextRef ctx = [context CGContext];
-    CGContextScaleCTM(ctx, 1.0, 1.0);
-
-    // Render the label into the context
-    [self.label.layer renderInContext:ctx];
-
-    [NSGraphicsContext restoreGraphicsState];
-
-    // Create the bitmap representation from the graphics context
-    NSBitmapImageRep *bitmapRep = [[NSBitmapImageRep alloc] initWithCGImage:[context.CGContext
-                                                                             createCGImage:[context.bitmapImageRep CGImage]
-                                                                             fromRect:[context.bitmapImageRep rect]]];
-
-    // Iterate through each cell and sample the color at the center
-    for (NSInteger row = 0; row < numRows; row++) {
-        for (NSInteger col = 0; col < numCols; col++) {
-            CGFloat x = bounds.origin.x + (col + 0.5) * cellWidth + self.originX;
-            CGFloat y = bounds.origin.y + (row + 0.5) * cellHeight + self.originY;
-
-            // Check if the color at the center of the cell is dark
-            BOOL isDarkColor = [viewController isColorAtPointDark:NSMakePoint(x, y) inBitmap:bitmapRep];
-
-            // Do something with the result (e.g., print it)
-            NSLog(@"Is color at cell (%ld, %ld) dark? %@", (long)row, (long)col, isDarkColor ? @"YES" : @"NO");
-        }
-    }
-}
-#endif
-
-
-
-- (void)sampleColorsInGridCells
+- (NSArray*)sampleColorsInGridCells
 {
+  NSMutableArray* cols = [NSMutableArray array];
+
   // Get the bounds of the view
   NSRect bounds = [self.samplingGrid bounds];
 
-  NSInteger numRows = 10;
-  NSInteger numCols = 10;
+  NSInteger numRows = (bounds.size.height-self.samplingGrid.originY)/self.samplingGrid.cellSize;
+  NSInteger numCols = (bounds.size.width-self.samplingGrid.originX)/self.samplingGrid.cellSize;
 
-  // Create the bitmap representation
-  NSBitmapImageRep *bitmapRep;
+  // Get the content of the label's layer as an image
+  NSImage *image = [[NSImage alloc] initWithSize:self.outputLabel.bounds.size];
+  [image lockFocus];
+  [self.outputLabel.layer renderInContext:[NSGraphicsContext currentContext].CGContext];
+  [image unlockFocus];
 
-  // Create a graphics context with a scale of 1.0
-  NSGraphicsContext *context = [NSGraphicsContext graphicsContextWithBitmapImageRep:[[NSBitmapImageRep alloc] initWithData:[NSData data]]];
-  [NSGraphicsContext saveGraphicsState];
-  [NSGraphicsContext setCurrentContext:context];
-  CGContextRef ctx = [context CGContext];
-  CGContextScaleCTM(ctx, 1.0, 1.0);
+  NSBitmapImageRep *bitmapRep = [[NSBitmapImageRep alloc] initWithData:[image TIFFRepresentation]];
 
-  // Render the label into the context
-  [self.outputLabel.layer renderInContext:ctx];
+  // Log the contents of the image (debugging purposes)
+//  NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+//  NSString *documentsDirectory = [paths firstObject];
+//  NSString *filePath = [documentsDirectory stringByAppendingPathComponent:@"debug_image.png"];
+//  NSData *pngData = [bitmapRep representationUsingType:NSBitmapImageFileTypePNG properties:@{}];
+//  [pngData writeToFile:filePath atomically:YES];
 
-  [NSGraphicsContext restoreGraphicsState];
-
-  // Retrieve the bitmap representation from the context
-  bitmapRep = [context.bitmapImageRep copy];
-
-//  // Get the content of the label's layer as an image
-//  NSImage *image = [[NSImage alloc] initWithSize:self.outputLabel.bounds.size];
-//
-//  [image lockFocus];
-//  [self.outputLabel.layer renderInContext:[NSGraphicsContext currentContext].CGContext];
-//  [image unlockFocus];
-//
-//  NSBitmapImageRep *bitmapRep = [[NSBitmapImageRep alloc] initWithData:[image TIFFRepresentation]];
-//
-//  // Log the contents of the image (debugging purposes)
-////  NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-////  NSString *documentsDirectory = [paths firstObject];
-////  NSString *filePath = [documentsDirectory stringByAppendingPathComponent:@"debug_image.png"];
-////  NSData *pngData = [bitmapRep representationUsingType:NSPNGFileType properties:@{}];
-////  [pngData writeToFile:filePath atomically:YES];
-
+  // get the scaling, might be retina as we are rendering in the window context, apparently
+  NSWindow *mainWindow = [[NSApplication sharedApplication] mainWindow];
+  CGFloat scale = [mainWindow backingScaleFactor];
 
   // Iterate through each cell and sample the color at the center
   NSMutableString* chartext = [NSMutableString string];
-  for (NSInteger row = 0; row < numRows; row++) {
-    for (NSInteger col = 0; col < numCols; col++) {
-      CGFloat x = bounds.origin.x + ((CGFloat)col + 0.5) * self.samplingGrid.cellSize + self.samplingGrid.originX;
-      CGFloat y = bounds.origin.y + ((CGFloat)row + 0.5) * self.samplingGrid.cellSize + self.samplingGrid.originY;
+  NSInteger glyphWidth = 0;
+  NSInteger glyphHeight = 0;
+  for (NSInteger col = 0; col < numCols; col++) {
+    [chartext appendFormat:@"%02ld: ", (long)col];
+    NSMutableArray* rowpixels = [NSMutableArray array];
+    for (NSInteger row = 0; row < numRows; row++) {
+      CGFloat x = bounds.origin.x + (((CGFloat)col + 0.5) * self.samplingGrid.cellSize + self.samplingGrid.originX) * scale;
+      CGFloat y = bounds.origin.y + (((CGFloat)row + 0.5) * self.samplingGrid.cellSize + self.samplingGrid.originY) * scale;
 
-      // Sample the color at the center of the cell
-      BOOL isDark = [self isColorAtPointDark:NSMakePoint(x, y) inBitmap:bitmapRep];
+      CGPoint pt = NSMakePoint(x, y);
+      // Note: NO NEED to Adjust the y-coordinate, apparently bitmapRep is also in Core Graphics orientation
+      //pt.y = bitmapRep.size.height*scale - pt.y;
+
+      // Sample the color from the image
+      BOOL isDark = [self isColorAtPointDark:pt inBitmap:bitmapRep];
+      if (isDark) glyphWidth = col+1;
+      if (isDark && row>=glyphHeight) glyphHeight = row+1;
       [chartext appendString:isDark ? @"X" : @"."];
+      [rowpixels addObject:@(isDark)];
 
       // Do something with the sampled color (e.g., print it)
       NSLog(@"Dark at cell (%ld, %ld) coord (%ld, %ld): %d", (long)row, (long)col, (long)x, (long)y, isDark);
 
     }
+    // end of row
     [chartext appendString:@"\n"];
+    [rowpixels removeObjectsInRange:NSMakeRange(glyphHeight, rowpixels.count-glyphHeight)];
+    [cols addObject:rowpixels];
   }
-  NSLog(@"Pixels:\n%@\n",chartext);
+  NSLog(@"Glyph (width:%ld, maxheight:%ld)\n%@\n", (long)glyphWidth, (long)glyphHeight, chartext);
+  // remove the extra cols
+  [cols removeObjectsInRange:NSMakeRange(glyphWidth, cols.count-glyphWidth)];
+  return cols;
 }
 
 
 - (BOOL)isColorAtPointDark:(NSPoint)point inBitmap:(NSBitmapImageRep *)bitmapRep
 {
-  // Sample the color at the specified point from the image
-  // Adjust the y-coordinate to match the image's coordinate system
-  CGFloat h = bitmapRep.size.height;
-  point.y = h - point.y;
-  NSLog(@"Smapling at bitmap coord (%ld, %ld) - bitmap size (%ld, %ld)", (long)point.x, (long)point.y, (long)bitmapRep.size.width, (long)bitmapRep.size.height);
-
   // Sample the color at the specified point from the existing bitmap
   NSColor *color = [bitmapRep colorAtX:point.x y:point.y];
 
